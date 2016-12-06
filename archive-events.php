92945fe84c47564e36d06dce93562500037717ca
jeroen <Jeroen@QForma.nl>
2016-12-06 12:37:19 +0100

    Styling

--- archive-events.php
+++ archive-events.php
@@ -1,6 +1,6 @@
 <?php
 $hasUpcomingEvents = false;
-$hasPastEvents = false;
+$hasPastEvents     = false;
 while (have_posts()) {
     the_post();
     if (get_post_meta(get_the_ID(), 'start_date', true) < date("Y-m-d")) {
@@ -9,51 +9,85 @@ while (have_posts()) {
         $hasUpcomingEvents = true;
     }
 }
-get_header(); ?>
-    <header class="full-width-entry-header mui--visible-xs-block">
-        <h1 class="entry-title mui--z2">Events</h1>
-    </header><!-- .entry-header -->
-    <div id="page" class="container mui-container">
-        <div class="mui-col-xs-12 <?php if (is_dynamic_sidebar()) {
-            echo "mui-col-md-9";
-        } ?>">
-            <header class="breaking-entry-header mui--hidden-xs">
-                <h1 class="entry-title mui--z2">Events</h1>
-            </header><!-- .entry-header -->
-            <div id="primary" class="content-area">
-                <main id="main" class="site-main" role="main">
-                    <?php if (have_posts()) :
-                        ?>
-                        <div style="padding: 10px;" class="mui--hidden-xs">
 
-                            <table style="width: 100%;" class="sortable">
+get_header() ?>
+    <div id="page" class="container">
+        <div class="row">
+            <div class="col s12 <?= is_dynamic_sidebar() ? 'col m8 l9' : '' ?>">
+                <div id="primary" class="content-area">
+                    <main id="main" class="site-main" role="main">
+                        <?php if (have_posts()) : ?>
+                            <?php
+                            #region Normal View
+                            ?>
+                            <div class="hide-on-small-only">
+                                <h1>Upcoming Events</h1>
+                                <?php if ($hasUpcomingEvents) : ?>
+                                    <table>
+                                        <tr>
+                                            <th>Title</th>
+                                            <th class="mui--hidden-sm mui--hidden-md">When</th>
+                                            <th style="text-align: center;">Banner</th>
+                                        </tr>
+                                        <?php
+                                        $upcomingEvents = array();
+                                        $args           = array(
+                                            'posts_per_page' => 10,
+                                            'paged'          => get_query_var('paged'),
+                                            'post_type'      => 'events',
+                                            'meta_key'       => 'start_date',
+                                            'orderby'        => 'meta_value',
+                                        );
+                                        $my_query       = new WP_Query($args);
+                                        while ($my_query->have_posts()) {
+                                            $my_query->the_post();
+                                            if (get_post_meta(get_the_ID(), 'start_date', true) >= date("Y-m-d")) {
+                                                ob_start();
+                                                ssv_get_event();
+                                                $upcomingEvents[] = ob_get_clean();
+                                            }
+                                        }
+                                        $upcomingEvents = array_reverse($upcomingEvents);
+                                        echo implode('', $upcomingEvents);
+                                        ?>
+                                    </table>
+                                <?php endif; ?>
+                                <?php
+                                if ($hasPastEvents) : ?>
+                                    <table>
+                                        <tr>
+                                            <th colspan="3"><h1>Past Events</h1></th>
+                                        </tr>
+                                        <tr>
+                                            <th>Title</th>
+                                            <th class="mui--hidden-sm mui--hidden-md">When</th>
+                                            <th style="text-align: center;">Banner</th>
+                                        </tr>
+                                        <?php
+                                        while (have_posts()) {
+                                            the_post();
+                                            if (get_post_meta(get_the_ID(), 'start_date', true) < date("Y-m-d")) {
+                                                ssv_get_event();
+                                            }
+                                        }
+                                        ?>
+                                    </table>
+                                <?php endif; ?>
+                            </div>
+                            <?php
+                            #endregion
+                            #region Mobile View
+                            ?>
+                            <div class="hide-on-med-and-up">
                                 <?php
                                 if ($hasUpcomingEvents) {
-                                    ?>
-                                    <tr>
-                                        <th colspan="3"><h1>Upcoming Events</h1></th>
-                                    </tr>
-                                    <tr>
-                                        <th>Title</th>
-                                        <th class="mui--hidden-sm mui--hidden-md">When</th>
-                                        <th style="text-align: center;">Banner</th>
-                                    </tr>
-                                    <?php
-
+                                    ?><h1>Upcoming Events</h1><?php
                                     $upcomingEvents = array();
-                                    $args = array(
-                                        'posts_per_page' => 10,
-                                        'paged'          => get_query_var('page'),
-                                        'post_type'      => 'events',
-                                        'meta_key'       => 'start_date',
-                                        'orderby'        => 'meta_value'
-                                    );
-                                    $my_query = new WP_Query($args);
-                                    while ($my_query->have_posts()) {
-                                        $my_query->the_post();
+                                    while (have_posts()) {
+                                        the_post();
                                         if (get_post_meta(get_the_ID(), 'start_date', true) >= date("Y-m-d")) {
                                             ob_start();
-                                            ssv_get_event();
+                                            ssv_get_xs_event();
                                             $upcomingEvents[] = ob_get_clean();
                                         }
                                     }
@@ -61,80 +95,28 @@ get_header(); ?>
                                     echo implode('', $upcomingEvents);
                                 }
                                 if ($hasPastEvents) {
-                                    ?>
-                                    <tr>
-                                        <th colspan="3"><h1>Past Events</h1></th>
-                                    </tr>
-                                    <tr>
-                                        <th>Title</th>
-                                        <th class="mui--hidden-sm mui--hidden-md">When</th>
-                                        <th style="text-align: center;">Banner</th>
-                                    </tr>
-                                    <?php
+                                    ?><h1>Past Events</h1><?php
                                     while (have_posts()) {
                                         the_post();
                                         if (get_post_meta(get_the_ID(), 'start_date', true) < date("Y-m-d")) {
-                                            ssv_get_event();
+                                            ssv_get_xs_event();
                                         }
                                     }
                                 }
                                 ?>
-                            </table>
-                        </div>
-                        <div class="mui--visible-xs-block">
+                            </div>
                             <?php
-                            if ($hasUpcomingEvents) {
-                                ?>
-                                <h1>Upcoming Events</h1>
-                                <?php
-                                $upcomingEvents = array();
-                                while (have_posts()) {
-                                    the_post();
-                                    if (get_post_meta(get_the_ID(), 'start_date', true) >= date("Y-m-d")) {
-                                        ob_start();
-                                        ssv_get_xs_event();
-                                        $upcomingEvents[] = ob_get_clean();
-                                    }
-                                }
-                                $upcomingEvents = array_reverse($upcomingEvents);
-                                echo implode('', $upcomingEvents);
-                            }
-                            if ($hasPastEvents) {
-                                ?>
-                                <h1>Past Events</h1>
-                                <?php
-                                while (have_posts()) {
-                                    the_post();
-                                    if (get_post_meta(get_the_ID(), 'start_date', true) < date("Y-m-d")) {
-                                        ssv_get_xs_event();
-                                    }
-                                }
-                            }
-                            ?>
-                        </div>
-                        <?php
-
-                        // Previous/next page navigation.
-                        the_posts_pagination(
-                            array(
-                                'prev_text'          => __('Previous page', 'ssv'),
-                                'next_text'          => __('Next page', 'ssv'),
-                                'before_page_number' => '<span class="meta-nav screen-reader-text">' . __(
-                                        'Page', 'ssv'
-                                    ) . ' </span>',
-                            )
-                        );
-
-                    // If no content, include the "No posts found" template.
-                    else :
-                        get_template_part('template-parts/content', 'none');
-                    endif;
-                    ?>
-
-                </main><!-- .site-main -->
-            </div><!-- .content-area -->
+                            #endregion
+                            echo mp_ssv_get_pagination();
+                        else :
+                            get_template_part('template-parts/content', 'none');
+                        endif;
+                        ?>
+                    </main>
+                </div>
+            </div>
+            <?php get_sidebar(); ?>
         </div>
-        <?php get_sidebar(); ?>
     </div>
 <?php get_footer(); ?>
 
@@ -163,7 +145,7 @@ function ssv_get_event()
             } else {
                 /** @noinspection SpellCheckingInspection */
                 echo '<a href="' . get_permalink(get_the_ID())
-                    . '"><img src="https://placeholdit.imgix.net/~text?txtsize=150&txt=No%20Banner%20Set&w=1920&h=480"/></a>';
+                     . '"><img src="https://placeholdit.imgix.net/~text?txtsize=150&txt=No%20Banner%20Set&w=1920&h=480"/></a>';
             } ?>
         </td>
     </tr>
@@ -173,16 +155,16 @@ function ssv_get_event()
 function ssv_get_xs_event()
 {
     $thumb = wp_get_attachment_image_src(get_post_thumbnail_id(), 'ssv-banner-s');
-    $url = $thumb[0];
+    $url   = $thumb[0];
     ?>
     <a href="<?php echo get_permalink(get_the_ID()); ?>">
         <div style="text-align: center;
-            margin: 10px;
-            padding-bottom: 25%;
-            position: relative;
-            background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-            url(<?php echo $url; ?>);
-            background-size: cover;">
+                margin: 10px;
+                padding-bottom: 25%;
+                position: relative;
+                background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+                url(<?php echo $url; ?>);
+                background-size: cover;">
             <h1 style="position: absolute; top: 50%; margin-top: -1.6em; width: 100%;  color: #ffffff;">
                 <?php the_title(); ?>
             </h1>
