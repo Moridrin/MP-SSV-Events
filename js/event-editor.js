a777bc254ba3312a3a78e714a630e50f618fa17f
Jeroen Berkvens <Account@Moridrin.com>
2018-01-09 08:40:31 +0100

    * Reset after Tests

--- js/event-editor.js
+++ js/event-editor.js
@@ -1,5 +1,6 @@
 // noinspection JSUnresolvedVariable
 let nextTicketId = data.ticketsMaxId;
+console.log('reset after tests');
 let ticketsManager = {
     addNew: function () {
         let ticketsTable = document.getElementById('the-list');
